1e8cd9fcc3b58edcb5d813416f890711222de079
(i1anzu) <(s26040@gsm.hs.kr)>
2026-06-07 01:26:40 +0900

    🐿️ :: window 사용

--- src/App.jsx
+++ src/App.jsx
@@ -46,11 +46,10 @@ async function hashPassword(password) {
   const encoder = new TextEncoder();
   const data = encoder.encode(password);
 
-  const hashBuffer = await crypto.subtle.digest(
+  const hashBuffer = await window.crypto.subtle.digest(
     'SHA-256',
     data
   );
-
   return Array.from(new Uint8Array(hashBuffer))
     .map((b) => b.toString(16).padStart(2, '0'))
     .join('');
