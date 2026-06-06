a610006773ba012131b3a8cbeb97a2110655311c
(i1anzu) <(s26040@gsm.hs.kr)>
2026-06-07 01:33:31 +0900

    🦭 :: 수정사항 반영 완료

--- src home/App.jsx
+++ src home/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import FabMenu from './components/FabMenu';
